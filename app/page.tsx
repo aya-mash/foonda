bdb18dfa6e00e3e6eb7ded359f11d819d5046618
AYABULELA YANELA MAHLATHINI <ayabulela_yanela.mahlathini@mercedes-benz.com>
2025-02-07 19:38:08 +0200

    setting up nextjs shadui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>Follow @aya-mash</div>;
-}
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <>
+      <div>Follow @aya-mash</div>
+      <Button>Follow</Button>
+    </>
+  );
+};
+
+export default Home;
---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between">
+      <h1 className="text-3xl font-bold">Foonda</h1>
+    </div>
+  );
+};
+
+export default Header;
